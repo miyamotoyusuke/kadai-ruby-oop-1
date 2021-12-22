815b023ff36bbe0274891c73f5961727a5612d65
miyamotoyusuke <miyamotoyusuke15@gmail.com>
2021-12-22 06:36:38 +0000

    1st commit MSG

---
+++ team.rb
@@ -0,0 +1,32 @@
+class Team
+  attr_accessor :name, :win, :lose, :draw
+    
+  def initialize(name, win, lose, draw)
+    self.name = name
+    self.win = win
+    self.lose = lose
+    self.draw = draw
+  end
+  
+  def calc_win_rate
+    win.to_f / (lose.to_f + win.to_f)
+  end
+  
+  def show_team_result
+    puts "#{@name}の2020年の成績は#{@win}勝#{@lose}敗#{@draw}分、勝率は#{calc_win_rate}です。"
+  end
+end
+
+team1 = Team.new('Giants', 67, 45, 8)
+team2 = Team.new('tigers', 60, 57, 3)
+team3 = Team.new('Dragons', 60, 55, 5)
+team4 = Team.new('BayStars', 56, 58 ,6)
+team5 = Team.new('Crap', 52, 56, 12)
+team6 = Team.new('Swallows', 41, 69, 10)
+
+team1.show_team_result
+team2.show_team_result
+team3.show_team_result
+team4.show_team_result
+team5.show_team_result
+team6.show_team_result
